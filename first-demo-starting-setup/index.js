7ea17ee23a4a12f3ec6aa513d87591426a27e590
bobadevis <88775833+bobadevis@users.noreply.github.com>
2022-05-09 21:57:20 +0530

    Delete first-demo-starting-setup directory

--- first-demo-starting-setup/index.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-    const app= express();
-    app.get('/',(req ,res )=>{res.send("MC , BC @Sagar & Daulat");});
-    app.listen(8080,()=>{console.log('listning on port 8080');});
